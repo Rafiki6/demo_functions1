5957a77a6b63ff52627a416786c1484de1208ba0
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-23 11:25:43 -0500

    function

---
+++ simple_functions.js
@@ -0,0 +1,17 @@
+// first exercise
+function favoriteThings(){
+    console.log("Muhamad Rafiki")
+    console.log("Black Panther")
+    console.log("Lil Wayne")
+}
+favoriteThings()
+// sec exercise
+function whyImHere(){
+    console.log("Because I want to have a career")
+}
+whyImHere()
+// third exercise
+function favoritePlace(){
+    console.log("I wolud love a Gorillars Park")
+}
+favoritePlace()
