724b217a8908bc6e53631501eb933f9fa1127832
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-23 11:04:52 -0500

    working on function

---
+++ demoFunction.js
@@ -0,0 +1,14 @@
+// DEFINE A FUNCTION
+function greet(name,kind){
+    // return kind +" " +name;
+    return `${kind} , ${name}`
+}
+console.log(greet("Joe","Rafiki"))
+
+function displayWeekdayMottos() {
+    console.log("Make it Happen Monday!")
+    console.log("Tackle it Tuesday!")
+    console.log("Finish it Friday!")
+   }
+    
+   displayWeekdayMottos();
