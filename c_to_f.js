266eb2e8d7e37d15723d7b674bf70578722593dd
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-25 21:05:51 -0500

    move to another folder

--- c_to_f.js
+++
@@ -1,8 +0,0 @@
-// temperature from Celsius to Fahrenheit 
-function convertCtoF(currentTemp){
-    let FahrenheitTemp = (currentTemp * 1.8) + 32;
-    return FahrenheitTemp;
-}
-let currentTemp = 98;
-let FahrenheitTemp = convertCtoF(currentTemp);
-console.log(FahrenheitTemp)
